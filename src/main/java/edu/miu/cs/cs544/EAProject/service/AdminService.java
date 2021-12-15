cd4ba9fe5dd67fc238cd700ebc6478796f6c8043
Wendessen <wendessen.ab@gmail.com>
2021-12-15 16:54:05 -0600

    impl course assignments

--- src/main/java/edu/miu/cs/cs544/EAProject/service/AdminService.java
+++ src/main/java/edu/miu/cs/cs544/EAProject/service/AdminService.java
@@ -1,5 +1,13 @@
 package edu.miu.cs.cs544.EAProject.service;
 
+import edu.miu.cs.cs544.EAProject.domain.CourseOffering;
+import edu.miu.cs.cs544.EAProject.domain.Student;
+
 public interface AdminService {
-    void processRegistration();
+
+    void processLatestEventRegistration();
+
+    void processRegistration(int eventId);
+
+    void assignStudentCourse(Student student, CourseOffering courseOffering);
 }
--- src/main/java/edu/miu/cs/cs544/EAProject/service/EventService.java
+++ src/main/java/edu/miu/cs/cs544/EAProject/service/EventService.java
@@ -8,12 +8,19 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface EventService {
+
     Page<RegistrationEvent> getAllEvents(Pageable pageable);
+
     RegistrationEvent getEventById(Integer id);
+
     RegistrationEvent createEvent(RegistrationEvent event);
+
     RegistrationEvent updateEvent(RegistrationEvent event);
+
     void deleteEvent(Integer id);
+
     RegistrationEvent getLatestEvent();
+
     List<AcademicBlock> getEventAcademicBlock(RegistrationEvent event);
 
 }
